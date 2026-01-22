f890dfab3015fc16c0dcf8f7509d3d1f0e3e1db6
watchara54692 <watchara54692@gmail.com>
2026-01-22 20:58:38 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,47 +1,54 @@
-
+import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from config import DATABASE_URL
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def get_db():
     return psycopg2.connect(
         DATABASE_URL,
+        sslmode="require",
         cursor_factory=RealDictCursor
     )
 
 
 def init_db():
-    with get_db() as conn:
-        conn.execute("""
-        CREATE TABLE IF NOT EXISTS chats(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+    conn = get_db()
+    cur = conn.cursor()
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS chats (
+            id SERIAL PRIMARY KEY,
             sender_id TEXT,
             page_id TEXT,
             message TEXT,
             sender_type TEXT,
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-        )
-        """)
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    """)
 
-        conn.execute("""
-        CREATE TABLE IF NOT EXISTS users(
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS users (
             sender_id TEXT PRIMARY KEY,
             first_name TEXT,
             profile_pic TEXT,
             ai_tag TEXT DEFAULT 'ยังไม่ชัดเจน',
             ai_chance INTEGER DEFAULT 0,
             ai_budget TEXT DEFAULT 'ไม่ระบุ',
-            last_ai_check DATETIME,
-            last_active DATETIME DEFAULT CURRENT_TIMESTAMP
-        )
-        """)
+            last_active TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    """)
 
-        conn.execute("""
-        CREATE TABLE IF NOT EXISTS knowledge(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS knowledge (
+            id SERIAL PRIMARY KEY,
             keyword TEXT,
             analysis TEXT,
             option_1 TEXT,
             option_2 TEXT
-        )
-        """)
+        );
+    """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
