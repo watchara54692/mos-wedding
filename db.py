852012f1665aadb0b1c27e5c0b8a0c3645473e32
watchara54692 <watchara54692@gmail.com>
2026-01-22 22:44:20 +0700

    Update db.py

--- db.py
+++ db.py
@@ -5,16 +5,14 @@ from psycopg2.pool import SimpleConnectionPool
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 pool = SimpleConnectionPool(
-    minconn=1,
-    maxconn=5,
+    1,
+    5,
     dsn=DATABASE_URL,
     sslmode="require"
 )
 
-
 def get_conn():
     return pool.getconn()
-
 
 def release_conn(conn):
     pool.putconn(conn)
