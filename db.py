3e5fca45b5e2ad280715a570e162f8e7ff548927
watchara54692 <watchara54692@gmail.com>
2026-01-22 21:59:02 +0700

    Update db.py

--- db.py
+++ db.py
@@ -18,22 +18,3 @@ def get_conn():
 
 def release_conn(conn):
     pool.putconn(conn)
-
-
-def init_db():
-    conn = get_conn()
-    cur = conn.cursor()
-
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS customers (
-        id SERIAL PRIMARY KEY,
-        name TEXT,
-        phone TEXT,
-        note TEXT,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-    """)
-
-    conn.commit()
-    cur.close()
-    release_conn(conn)
