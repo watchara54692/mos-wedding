7ab78d822b771348f0cf12316b310c6fb449b9a2
watchara54692 <watchara54692@gmail.com>
2026-01-22 19:57:19 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os, json
+from datetime import timedelta
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "mos_secret")
+
+ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "mos1234")
+
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+
+FB_VERIFY_TOKEN = os.environ.get("FB_VERIFY_TOKEN")
+
+PAGE_TOKENS = json.loads(os.environ.get("PAGE_TOKENS", "{}"))
+
+DATABASE_PATH = os.environ.get("DATABASE_PATH", "mos_chat.db")
+
+SESSION_LIFETIME = timedelta(days=365)
